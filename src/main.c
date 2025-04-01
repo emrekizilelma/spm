b8b55b3b0eb67e35b00da1ab94f62277a04d84f1
emrekizilelma <emrekizilelma00@gmail.com>
2025-04-01 13:05:52 +0300

    add: spm layout

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 struct SPM {
     const char* name;
@@ -21,7 +22,24 @@ void help_menu(void) {
     printf("  --remove  <package>\tRemove the package\n\n");
 }
 int main(int argc, char* argv[]) {
-    
-    help_menu();
+    if(argc == 1) { printf("[ERROR] No arguments passed! Please see by typing spm help.\n\n"); }
+
+    else if(strcmp(argv[1], "help") == 0) { help_menu(); }
+    else if(strcmp(argv[1], "update") == 0) { printf("Checking the update...\n"); }
+    else if(strcmp(argv[1], "version") == 0) { printf("%s\n", spm.version); }
+    else if(strcmp(argv[1], "install") == 0) {
+        if(argc < 3) {
+            printf("missing <package> line\n");
+        }
+        else { printf("Installing package... '%s'\n", argv[2]);}
+    }
+    else if(strcmp(argv[1], "remove") == 0) {
+        if(argc < 3){
+            printf("Missing <package> line\n");
+        }
+        else { printf("Removing package...'%s'\n", argv[2]); } }
+
+    else { printf("Unknown command '%s' See spm help\n", argv[1]); }
+
     return 0;
 }
